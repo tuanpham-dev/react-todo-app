3f58d5ffa5cafd9506323096f0d201e0b99b7e82
Tuan Pham <tuanpham.developer@gmail.com>
2020-06-26 12:01:03 -0400

    Add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Todos from './components/Todos/Todos'
+import { GlobalStyles } from './globalStyles'
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyles />
       <Todos />
     </div>
   )
---
+++ src/globalStyles.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+  body {
+    font-family: 'Nunito', sans-serif;
+    font-size: 20px;
+    background: #fafafa;
+    color: #515056;
+
+    * {
+      box-sizing: border-box;
+    }
+  }
+`
