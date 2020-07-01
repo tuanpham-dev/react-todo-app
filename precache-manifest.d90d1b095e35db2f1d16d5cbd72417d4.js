73b66d30fc056a501cbe96d79a27ffbf0b6e9edc
Tuan Pham <tuanpham.developer@gmail.com>
2020-07-01 15:01:07 -0400

    Updates

---
+++ precache-manifest.d90d1b095e35db2f1d16d5cbd72417d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdee9d3fb04e7b10ef6e5d531d6d5b58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ab638a9480b9e2a4649",
+    "url": "/static/js/2.8c1f7d71.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.8c1f7d71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b5d8382f7efa540c2b9",
+    "url": "/static/js/main.b3c67793.chunk.js"
+  },
+  {
+    "revision": "84e0579a32d12ba8be85",
+    "url": "/static/js/runtime-main.05dcf7c1.js"
+  }
+]);
